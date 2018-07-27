fe68d21012630ee5ce0bd11160b93e4985791875
Geneviève Bastien <gbastien+lttng@versatic.net>
2018-07-27 12:36:06 -0400

    Add a setup.py for easy installation
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
 # lttng-utils
 This repository contains helper scripts to record traces with LTTng for many common use cases.
 
+### Install
+
+To install the helper script and profiles, run the setup script
+
+```
+$ sudo python3 setup.py install
+```
+
 ### Usage
 
 To simply record a trace with common kernel events that can then be analyzed with most of the analyses in [lttng-analyses](https://github.com/lttng/lttng-analyses) or opened in [Trace Compass](http://tracecompass.org). Tracing can be stopped by pressing ctrl-c.
 
 ```
-$ ./lttng-record-trace
+$ lttng-record-trace
 ```
 
 To trace a specific application, just add the application to trace to the arguments of the commands, for example, to trace only the *ls -alt* command
 
 ```
-$ ./lttng-record-trace ls -alt
+$ lttng-record-trace ls -alt
 ```
 
 More options are available for tracing. See the helper
 
 ```
-$ ./lttng-record-trace --help
+$ lttng-record-trace --help
 ```
---
+++ setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+#
+# The MIT License (MIT)
+#
+# Copyright (C) 2018 - Geneviève Bastien <gbastien@versatic.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+"""LTTng-utils setup script"""
+
+import shutil
+import sys
+from setuptools import setup
+
+if sys.version_info[0:2] < (3, 4):
+    raise RuntimeError("Python version >= 3.4 required.")
+
+if 'install' in sys.argv:
+    if shutil.which('lttng') is None:
+        print('lttng-utils is a helper for lttng and lttng should be available on the system.\n'
+              'See https://www.lttng.org for more info.',
+              file=sys.stderr)
+        sys.exit(1)
+
+setup(
+    name='lttng-utils',
+    version="0.1",
+
+    description='LTTng-utils provides helper scripts for lttng tracing',
+
+    url='https://github.com/tahini/lttng-utils',
+
+    author='Geneviève Bastien',
+    author_email='gbastien@versatic.net',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Monitoring',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+    ],
+
+    keywords='lttng tracing',
+
+    packages=[
+        'lttngprofiles',
+        ],
+
+    scripts=[
+        'lttng-record-trace'
+    ],
+
+    include_package_data = True
+
+)
